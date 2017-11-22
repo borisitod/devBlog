0903d0638829ade04b37695d2886ad5b1a0bbd0b
Boris <boris.chen@kdis.com>
2017-11-22 13:45:57 +1100

    code cleanup at the moment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,56 +12,17 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('styles')
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar has-shadow is-white" role="navigation" aria-label="main navigation">
-            <div class="container">
-                <div class="navbar-start">
-                    <a class="navbar-item" href="{{route('home')}}">
-                        <img src="{{asset('images/devblog-logo.png')}}" alt="DevBlog" />
-                    </a>
-                    <a href="#" class="navbar-item is-tab is-hidden-mobile">Learn</a>
-                    <a href="#" class="navbar-item is-tab is-hidden-mobile">Discuss</a>
-                    <a href="#" class="navbar-item is-tab is-hidden-mobile">Share</a>
-                </div>
-                <div class="navbar-end">
-                    @if (Auth::guest())
-                        <a href="{{route('login')}}" class="navbar-item is-tab">Login</a>
-                        <a href="{{route('register')}}" class="navbar-item is-tab">Join the Community</a>
-                        @else
-                        <div class="navbar-item has-dropdown is-active">
-                            <a class="navbar-link">
-                               Hi Boris
-                            </a>
 
-                            <div class="navbar-dropdown">
-                                <a class="navbar-item">
-                                    <span class="icon"><i class="fa fa-fw fa-user-circle-o"></i></span>
-                                    Profile
-                                </a>
-                                <a class="navbar-item">
-                                    <span class="icon"><i class="fa fa-fw fa-bell"></i></span>
-                                    Notifications
-                                </a>
-                                <a class="navbar-item">
-                                    <span class="icon"><i class="fa fa-fw fa-cog"></i></span>
-                                    Settings
-                                </a>
-                                <hr class="navbar-divider">
-                                <a class="navbar-item">
-                                    <span class="icon"><i class="fa fa-sign-out "></i></span>
-                                    Logout
-                                </a>
-                            </div>
-                    @endif
-                </div>
-            </div>
-        </nav>
+    @include('_includes.nav.main')
+    <div id="app">
         @yield('content')
     </div>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    @yield('scripts')
 </body>
 </html>
