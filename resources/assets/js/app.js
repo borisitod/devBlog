c92138bfb7644c0cd6e39cdb9a59ecd0f35bddc2
Boris <boris.chen@kdis.com>
2017-12-08 12:08:05 +1100

    add slug for the posts

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,11 +1,14 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+window.Slug = require('slug');
+Slug.defaults.mode = 'rfc3986';
 
 import Buefy from 'buefy';
 
 
 Vue.use(Buefy);
+Vue.component('slugWidget', require('./components/slugWidget.vue'));
 
 // Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
